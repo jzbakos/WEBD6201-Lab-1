141b96f78d39475896c5132332ac0536043c1e4d
Josh Bakos <jzbakos@gmail.com>
2020-01-24 22:23:36 -0500

    Added javascript functionality.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,8 +1,46 @@
+/**
+ * Author: Josh Bakos
+ * Student ID: 100652490
+ * Date Completed: 2020-01-24
+ */
+
 (function() {
 
     function Start()
     {
         console.log("App started.");
+        Main();
+    }
+
+    function Main()
+    {
+        // Rename Products to Projects
+        let products = document.getElementsByClassName("nav-item")[1];
+        let projects_content = '<a class="nav-link" href="#"> <i class="fas fa-th"></i> Products</a>';
+        products.innerHTML = projects_content;
+
+        // Add new navbar choice
+        let navbar = document.getElementsByClassName("navbar-nav ml-auto")[0];
+        
+
+        // Creating the new nav tab
+        human_resources_tab = document.createElement('li');
+        human_resources_tab.setAttribute('class', 'nav-item');
+        // Create the inner HTML for the new li
+        let human_resources_content = ' <a class="nav-link" href="#"> <i class="fas fa-hands-helping"></i> Human Resources</a> ';
+        // Set the inner HTML
+        human_resources_tab.innerHTML = human_resources_content;
+        // Insert between About Us and Contact Us
+        navbar.insertBefore(human_resources_tab, navbar.children[4]);
+
+
+        // Creating new fixed-bottom navbar
+        bottom_nav = document.createElement('nav');
+        bottom_nav.setAttribute('class', 'navbar fixed-bottom navbar-dark bg-dark');
+        let bottom_nav_content = '<a class="navbar-brand" href="#">&copy Josh Bakos 2020</a>';
+        bottom_nav.innerHTML = bottom_nav_content;
+        document.body.appendChild(bottom_nav);
+        
     }
 
     window.addEventListener("load", Start);
