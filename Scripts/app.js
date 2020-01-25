a751afcb82d8318f03b900bf6ae9662f0ba7733d
Josh Bakos <jzbakos@gmail.com>
2020-01-24 22:29:52 -0500

    Added internal documentation and link redirect for footer.

--- Scripts/app.js
+++ Scripts/app.js
@@ -14,7 +14,9 @@
 
     function Main()
     {
-        // Rename Products to Projects
+        /**
+         * Rename Products to Projects
+         */
         let products = document.getElementsByClassName("nav-item")[1];
         let projects_content = '<a class="nav-link" href="#"> <i class="fas fa-th"></i> Products</a>';
         products.innerHTML = projects_content;
@@ -23,7 +25,9 @@
         let navbar = document.getElementsByClassName("navbar-nav ml-auto")[0];
         
 
-        // Creating the new nav tab
+        /**
+         * Creating the new nav tab
+         */
         human_resources_tab = document.createElement('li');
         human_resources_tab.setAttribute('class', 'nav-item');
         // Create the inner HTML for the new li
@@ -34,14 +38,18 @@
         navbar.insertBefore(human_resources_tab, navbar.children[4]);
 
 
-        // Creating new fixed-bottom navbar
+        /**
+         * Creating new fixed-bottom navbar
+         */
         bottom_nav = document.createElement('nav');
+        // Set nav class to fixed-bottom and dark themed
         bottom_nav.setAttribute('class', 'navbar fixed-bottom navbar-dark bg-dark');
-        let bottom_nav_content = '<a class="navbar-brand" href="#">&copy Josh Bakos 2020</a>';
+        // Create nav inner HTML content
+        let bottom_nav_content = '<a class="navbar-brand" href="http://www.linkedin.com/in/jzbakos/" target="_blank">&copy Josh Bakos 2020</a>';
+        // Set inner HTML
         bottom_nav.innerHTML = bottom_nav_content;
+        // Append the new navbar to the body
         document.body.appendChild(bottom_nav);
-        
     }
-
     window.addEventListener("load", Start);
 })()
